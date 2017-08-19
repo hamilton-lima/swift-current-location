0b109d5ad0d9387740f5b6a0e4b3d46a1157d86f
Hamilton Lima <hamilton.lima@gmail.com>
2017-08-19 18:49:53 -0400

    Example ready

--- MapDraft/ViewController.swift
+++ MapDraft/ViewController.swift
@@ -5,19 +5,61 @@
 //  Created by Hamilton Lima on 2017-08-19.
 //  Copyright © 2017 Hamilton Lima. All rights reserved.
 //
+//  
+//
+// add to Info.plist
+// - required device capabilities: gps, location-services
+// - NSLocationWhenInUseUsageDescription
+// - NSLocationAlwaysUsageDescription
+//
 
 import UIKit
+import Foundation
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+   
+    @IBOutlet weak var map: MKMapView!
+
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestLocation()
+            print("location services ENABLED.")
+        } else {
+            print("location services not enabled.")
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: NSError){
+        print("location services delegate call FAIL")
+    }
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        print("location services delegate call", locations)
+        
+        let location = locations.first! as CLLocation
+        print("location found", location)
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: true)
     }
 
 
